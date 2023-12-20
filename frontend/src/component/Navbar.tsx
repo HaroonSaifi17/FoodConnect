517c72717947a2d7ffcbbb4bae4512f5eff032ba
Mohd Haroon <haroonsaifi2001@gmail.com>
2023-12-20 22:21:38 +0530

    some basic experiment

---
+++ frontend/src/component/Navbar.tsx
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+  return (
+    <nav id='home'>
+        <div className='max-w-[1400px] mx-auto'>
+        <a href="#Home">Food Connect</a>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+        </ul>
+        </div>    
+    </nav>
+  )
+}
